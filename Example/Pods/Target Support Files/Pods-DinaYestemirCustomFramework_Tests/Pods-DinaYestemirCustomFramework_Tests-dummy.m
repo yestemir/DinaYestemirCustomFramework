0398453173b662fc7d52aa5ca53876fc6d01256f
Dina Yestemir <dina.yestemir@gmail.com>
2021-05-05 08:50:43 +0600

    initial comment

---
+++ Example/Pods/Target Support Files/Pods-DinaYestemirCustomFramework_Tests/Pods-DinaYestemirCustomFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DinaYestemirCustomFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DinaYestemirCustomFramework_Tests
+@end
